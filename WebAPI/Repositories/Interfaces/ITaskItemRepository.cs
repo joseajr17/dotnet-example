979ed90ffec398550e461451bb54920377dde27c
José Antonio <josejr017@gmail.com>
2026-02-23 10:54:15 -0300

    TaskItemRepository

---
+++ WebAPI/Repositories/Interfaces/ITaskItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebAPI.Models;
+
+namespace WebAPI.Repositories.Interfaces
+{
+    public interface ITaskItemRepository
+    {
+        Task<List<TaskItemModel>> GetAll();
+        Task<TaskItemModel> GetById(int id);
+        Task<TaskItemModel> Add(TaskItemModel taskItem);
+        Task<TaskItemModel> Update(TaskItemModel taskItem, int id);
+        Task<bool> Delete(int id);
+    }
+}
---
+++ WebAPI/Repositories/TaskItemRepository.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+using WebAPI.Models;
+using WebAPI.Repositories.Interfaces;
+
+namespace WebAPI.Repositories
+{
+    public class TaskItemRepository : ITaskItemRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public TaskItemRepository(AppDbContext appDbContext) {
+            _dbContext = appDbContext;
+        }
+
+        public async Task<List<TaskItemModel>> GetAll()
+        {
+            return await _dbContext.TaskItems.ToListAsync();
+        }
+
+        public async Task<TaskItemModel> GetById(int id)
+        {
+            return await _dbContext.TaskItems.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<TaskItemModel> Add(TaskItemModel taskItem)
+        {
+            await _dbContext.TaskItems.AddAsync(taskItem);
+            await _dbContext.SaveChangesAsync();
+            return taskItem;
+        }
+
+        public async Task<TaskItemModel> Update(TaskItemModel taskItem, int id)
+        {
+            TaskItemModel taskById = await GetById(id);
+
+            if (taskById == null) {
+                throw new Exception($"The task for ID: {id} was not found.");
+            }
+
+            taskById.Name = taskItem.Name;
+            taskById.Description = taskItem.Description;
+            taskById.Status = taskItem.Status;
+
+            _dbContext.Update(taskById);
+            await _dbContext.SaveChangesAsync();
+            return taskById;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            TaskItemModel taskById = await GetById(id);
+
+            if (taskById == null)
+            {
+                throw new Exception($"The task for ID: {id} was not found.");
+            }
+
+            _dbContext.TaskItems.Remove(taskById);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+
+        
+    }
+}
